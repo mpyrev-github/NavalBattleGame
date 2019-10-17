f7fbbf3e3a7d5d31eae8dad19d53ef7f8bb094ec
Mikhail Pyrev <mihail.pyrev@gmail.com>
2019-10-17 20:58:40 +0500

    Create an Area NavalBattle.

--- game.js
+++ game.js
@@ -1,3 +1,23 @@
+let userArea = document.getElementById('userArea');
+let aiArea = document.getElementById('aiArea');
+Create_Container(userArea,"grey");
+Create_Container(aiArea, "lightseagreen");
+function Create_Container(div, color) {
+    for (let j = 0; j < 10; j++) {
+        for (let i = 0; i < 10; i++) {
+            let inner_Div = document.createElement('div');
+            inner_Div.className='pos';
+            inner_Div.id = 'num'+(i+10*j+1);
+            inner_Div.style.padding = '20px';
+            inner_Div.style.display = 'inline-block';
+            inner_Div.style.background = color;
+            inner_Div.style.border = 'medium outset silver';
+            div.appendChild(inner_Div);
+        }
+    }
+}
+
+/* OLD GAME
 let randomLoc = Math.floor(Math.random() * 5);
 let location1 = randomLoc;
 let location2 = location1 + 1;
@@ -30,3 +50,4 @@ while (isSunk == false) {
 var stats = "You took " + guesses + " guesses to sink the battleship, " +
                     "which means your shooting accuracy was " + (3/guesses);
 alert(stats);
+*/
